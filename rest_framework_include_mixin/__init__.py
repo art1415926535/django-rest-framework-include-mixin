10447df6f38318710f0546fcbdadbe69abd647bf
art1415926535 <art1415926535@yandex.ru>
2018-12-09 21:21:36 +0300

    MANIFEST.in was added (include README.md) for Python 3.4 compatibility. universal=1 was added to bdist_wheel.

--- rest_framework_include_mixin/__init__.py
+++ rest_framework_include_mixin/__init__.py
@@ -2,7 +2,7 @@ from .mixins import SerializerIncludeMixin, QueryOptimizerMixin
 
 
 __title__ = 'Optimized includable serializer fields'
-__version__ = '0.3'
+__version__ = '0.4'
 __author__ = 'Artem Fedotov'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018 Artem Fedotov'
